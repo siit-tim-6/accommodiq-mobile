cd0b59451f70be409038128e3a3a3db08dec16ab
Teodor <teodorv22@gmail.com>
2023-11-07 00:30:17 +0100

    feat: add clikcListener to CreateAccount textView

--- app/src/main/java/com/example/accommodiq/LoginActivity.java
+++ app/src/main/java/com/example/accommodiq/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.accommodiq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        TextView signUpBtn = findViewById(R.id.createAccountTextView);
+        signUpBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
     }
 }
