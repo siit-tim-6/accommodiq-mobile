d468b66b5fd295514e6fefa94bd6e83aa4ded0bd
Teodor <teodorv22@gmail.com>
2023-11-06 12:20:23 +0100

    add: add LoginActivity and SplashScreenActivity class and xml files

---
+++ app/src/main/java/com/example/accommodiq/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.example.accommodiq;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        getSupportActionBar().hide();
+        int SPLASH_TIME_OUT=3000;
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreenActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+    }
+}
