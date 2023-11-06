c3a7b9faaad2453f719287afde8bc85c6a4af4af
Teodor <teodorv22@gmail.com>
2023-11-07 00:51:15 +0100

    feat: add views to welcome page and add onClickeListener to getStartet button

--- app/src/main/java/com/example/accommodiq/WelcomeScreenActivity.java
+++ app/src/main/java/com/example/accommodiq/WelcomeScreenActivity.java
@@ -2,7 +2,9 @@ package com.example.accommodiq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeScreenActivity extends AppCompatActivity {
 
@@ -10,5 +12,7 @@ public class WelcomeScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+        TextView signUpBtn = findViewById(R.id.getStartedBtn);
+        signUpBtn.setOnClickListener(view -> startActivity(new Intent(WelcomeScreenActivity.this,RegisterActivity.class)));
     }
 }
