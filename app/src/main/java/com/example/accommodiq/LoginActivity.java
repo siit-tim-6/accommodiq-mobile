c7cec08fc99913d2f036475aca8dadf9e6228afe
Teodor <teodorv22@gmail.com>
2023-11-07 11:45:46 +0100

    feat: add underline to createAccount and dependency in build gradle

--- app/src/main/java/com/example/accommodiq/LoginActivity.java
+++ app/src/main/java/com/example/accommodiq/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.accommodiq;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.text.HtmlCompat;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,7 +13,10 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        TextView signUpBtn = findViewById(R.id.createAccountTextView);
-        signUpBtn.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this,RegisterActivity.class)));
+        TextView createAccountTextView = findViewById(R.id.createAccountTextView);
+        createAccountTextView.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this,RegisterActivity.class)));
+
+        String text = "<u>" + createAccountTextView.getText().toString() + "</u>";
+        createAccountTextView.setText(HtmlCompat.fromHtml(text,HtmlCompat.FROM_HTML_MODE_LEGACY));
     }
 }
