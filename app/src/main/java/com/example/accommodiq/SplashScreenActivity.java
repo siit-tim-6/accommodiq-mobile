6b136ef8c669640834b3f2bb20e8812ea1fe78f3
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-08 21:53:43 +0100

    feat: add network connectivity check

--- app/src/main/java/com/example/accommodiq/SplashScreenActivity.java
+++ app/src/main/java/com/example/accommodiq/SplashScreenActivity.java
@@ -2,8 +2,13 @@ package com.example.accommodiq;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkCapabilities;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.widget.Toast;
 
 import java.util.Timer;
 import java.util.TimerTask;
@@ -14,15 +19,32 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
-        //getSupportActionBar().hide();
-        int SPLASH_TIME_OUT=3000;
-        new Timer().schedule(new TimerTask() {
-            @Override
-            public void run() {
-                Intent intent = new Intent(SplashScreenActivity.this, WelcomeScreenActivity.class);
-                startActivity(intent);
-                finish();
-            }
-        },SPLASH_TIME_OUT);
+        int SPLASH_TIME_OUT=5000;
+
+        if(isConnectedToInternet()){
+            new Timer().schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(SplashScreenActivity.this, WelcomeScreenActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },SPLASH_TIME_OUT);
+        }else {
+            Toast.makeText(this, "Device is not connected to the internet.", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(Settings.ACTION_WIFI_SETTINGS);
+            startActivity(intent);
+        }
+
+
+    }
+
+    private boolean isConnectedToInternet() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(connectivityManager.getActiveNetwork());
+
+        return networkCapabilities != null &&
+                (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) ||
+                        networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI));
     }
 }
