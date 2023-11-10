c60fc331f42f253fc5a226b4e1072e6169a00e88
Vladimir Popov <vladapopov02@gmail.com>
2023-11-10 15:50:42 +0100

    feat: added AccommodationListAdapter adapter for accommodation_card display

--- app/src/main/java/com/example/accommodiq/adapters/AccommodationListAdapter.java
+++ app/src/main/java/com/example/accommodiq/adapters/AccommodationListAdapter.java
@@ -1,2 +1,72 @@
-package com.example.accommodiq.adapters;public class AccommodationListAdapter {
+package com.example.accommodiq.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.accommodiq.R;
+import com.example.accommodiq.models.Accommodation;
+
+import java.util.ArrayList;
+
+public class AccommodationListAdapter extends ArrayAdapter<Accommodation> {
+    private ArrayList<Accommodation> accommodations;
+
+    public AccommodationListAdapter(Context context, ArrayList<Accommodation> accommodations) {
+        super(context, R.layout.accommodation_card, accommodations);
+        this.accommodations = accommodations;
+    }
+
+    @Override
+    public int getCount() {
+        return accommodations.size();
+    }
+
+    @Nullable
+    @Override
+    public Accommodation getItem(int position) {
+        return accommodations.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        Accommodation accommodation = getItem(position);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.accommodation_card, parent, false);
+        }
+        LinearLayout accommodationCard = convertView.findViewById(R.id.accommodation_card_item);
+        ImageView imageView = convertView.findViewById(R.id.accommodation_image);
+        TextView titleTextView = convertView.findViewById(R.id.accommodation_title);
+        RatingBar ratingBar = convertView.findViewById(R.id.accommodation_rating_stars);
+        TextView ratingTextView = convertView.findViewById(R.id.accommodation_rating);
+        TextView reviewCountTextView = convertView.findViewById(R.id.accommodation_review_count);
+        TextView locationTextView = convertView.findViewById(R.id.accommodation_location);
+
+        if (accommodation != null) {
+            String reviewCount = "(" + accommodation.getReviewCount() + ")";
+            imageView.setImageResource(accommodation.getImage());
+            titleTextView.setText(accommodation.getTitle());
+            ratingBar.setRating(((float) accommodation.getRating()));
+            ratingTextView.setText(String.valueOf(accommodation.getRating()));
+            reviewCountTextView.setText(reviewCount);
+            locationTextView.setText(accommodation.getLocation());
+        }
+
+        return convertView;
+    }
 }
