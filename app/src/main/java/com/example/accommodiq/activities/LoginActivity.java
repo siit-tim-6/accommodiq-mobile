767f966ee3c9a2506c54ab953e3a073ad4e9784a
Teodor Vidakovic <teodorv22@gmail.com>
2023-11-17 23:24:32 +0100

    refactor: refactor login activity

--- app/src/main/java/com/example/accommodiq/activities/LoginActivity.java
+++ app/src/main/java/com/example/accommodiq/activities/LoginActivity.java
@@ -16,17 +16,23 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
-        TextView createAccountTextView = findViewById(R.id.createAccountTextView);
-        createAccountTextView.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
-        TextUtilities.makeTextUnderlined(createAccountTextView);
-
-        ImageView closeBtn = findViewById(R.id.closeButton);
-        closeBtn.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, MainActivity.class)));
+        setUpCreateAccountTextView();
+        setUpCloseBtn();
     }
 
     public void logIn(View view) {
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+
+    private void setUpCreateAccountTextView() {
+        TextView createAccountTextView = findViewById(R.id.createAccountTextView);
+        createAccountTextView.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
+        TextUtilities.makeTextUnderlined(createAccountTextView);
+    }
+
+    private void setUpCloseBtn() {
+        ImageView closeBtn = findViewById(R.id.closeButton);
+        closeBtn.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, MainActivity.class)));
     }
 }
