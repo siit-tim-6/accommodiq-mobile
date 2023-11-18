746f50dd345e4b81b1ed1e6a4efa183c2d660752
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-11-18 20:35:32 +0100

    feat: Add different menus depending on Account role that logged in

--- app/src/main/java/com/example/accommodiq/activities/MainActivity.java
+++ app/src/main/java/com/example/accommodiq/activities/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.navigation.ui.NavigationUI;
 import com.example.accommodiq.R;
 import com.example.accommodiq.databinding.ActivityMainBinding;
 import com.example.accommodiq.models.Accommodation;
+import com.example.accommodiq.models.Account;
+import com.example.accommodiq.services.AccountService;
 
 import java.util.ArrayList;
 
@@ -24,6 +26,24 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        Account loggedInAccount = AccountService.getInstance().getLoggedInAccount();
+        if (loggedInAccount == null) {
+            binding.navView.getMenu().clear();
+            binding.navView.inflateMenu(R.menu.bottom_nav_menu_unauthorized);
+            return;
+        }
+        else {
+            switch (loggedInAccount.getAccountType()) {
+                case OWNER:
+                    binding.navView.getMenu().clear();
+                    binding.navView.inflateMenu(R.menu.bottom_nav_menu_owner);
+                    break;
+                case ADMIN:
+                    binding.navView.getMenu().clear();
+                    binding.navView.inflateMenu(R.menu.bottom_nav_menu_admin);
+                    break;
+            }
+        }
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
