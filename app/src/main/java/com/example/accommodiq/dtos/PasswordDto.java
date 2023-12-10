65d64da3724d3c4ae4a0aa6a1b903af7c29638a3
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-12-10 13:23:36 +0100

    feat: Create password related classes

---
+++ app/src/main/java/com/example/accommodiq/dtos/PasswordDto.java
@@ -0,0 +1,37 @@
+package com.example.accommodiq.dtos;
+
+import com.example.accommodiq.models.Password;
+
+public class PasswordDto {
+    private String oldPassword;
+    private String newPassword;
+
+    public PasswordDto() {
+    }
+
+    public PasswordDto(String oldPassword, String newPassword) {
+        this.oldPassword = oldPassword;
+        this.newPassword = newPassword;
+    }
+
+    public PasswordDto(Password password) {
+        this.oldPassword = password.getOldPassword();
+        this.newPassword = password.getNewPassword();
+    }
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+}
