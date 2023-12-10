ac481facd28cde047cab5bccf38c36c0ea1454db
BulatovicBalsa <baki.b.2k02@gmail.com>
2023-12-10 13:34:48 +0100

    feat: Add change password related methods and fields

--- app/src/main/java/com/example/accommodiq/ui/account/AuthorizedProfileViewModel.java
+++ app/src/main/java/com/example/accommodiq/ui/account/AuthorizedProfileViewModel.java
@@ -7,6 +7,7 @@ import androidx.annotation.NonNull;
 import androidx.databinding.Bindable;
 
 import com.example.accommodiq.clients.ClientUtils;
+import com.example.accommodiq.dtos.PasswordDto;
 import com.example.accommodiq.models.Account;
 
 import retrofit2.Call;
@@ -15,13 +16,15 @@ import retrofit2.Response;
 import androidx.databinding.BaseObservable;
 
 import com.example.accommodiq.BR;
+import com.example.accommodiq.models.Password;
 
 import java.util.Objects;
 
 public class AuthorizedProfileViewModel extends BaseObservable {
     private Account account = new Account();
     private Boolean showChangePassword = false;
-    private final String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJob3N0M0BleGFtcGxlLmNvbSIsImV4cCI6MTcwMjE5NTA2NiwiaWF0IjoxNzAyMTc3MDY2fQ.uyQn2nJmvuSwTNMJ3_BYUFFTgwS_fAyXaj82w45Ld7-wuKKFFDghUyEt0MJqY76eHby95jYYBl_wIran8dVWsQ";
+    private Password password = new Password();
+    private final String token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJob3N0M0BleGFtcGxlLmNvbSIsImV4cCI6MTcwMjIyOTYyMywiaWF0IjoxNzAyMjExNjIzfQ.iq0c9S_jpgpYJIAL0FN-btfnHFqYGed5KyyRBZS-oDGU-xi67G0WYEK_9yc9xLxFbNrFnjTgz5E_xF7Av3l5Cg";
 
     public AuthorizedProfileViewModel() {
         Call<Account> call = ClientUtils.accountService.getAccountDetails("Bearer " + token);
@@ -117,6 +120,39 @@ public class AuthorizedProfileViewModel extends BaseObservable {
             showChangePassword = value;
     }
 
+    @Bindable public String getOldPassword() {
+        return password.getOldPassword();
+    }
+
+    public void setOldPassword(String value) {
+        if (!Objects.equals(password.getOldPassword(), value))
+            password.setOldPassword(value);
+
+        notifyPropertyChanged(BR.oldPassword);
+    }
+
+    @Bindable public String getNewPassword() {
+        return password.getNewPassword();
+    }
+
+    public void setNewPassword(String value) {
+        if (!Objects.equals(password.getNewPassword(), value))
+            password.setNewPassword(value);
+
+        notifyPropertyChanged(BR.newPassword);
+    }
+
+    @Bindable public String getRepeatPassword() {
+        return password.getRepeatPassword();
+    }
+
+    public void setRepeatPassword(String value) {
+        if (!Objects.equals(password.getRepeatPassword(), value))
+            password.setRepeatPassword(value);
+
+        notifyPropertyChanged(BR.repeatPassword);
+    }
+
     public void updateAccountDetails(View view) {
         String email = getEmail();
         String firstName = getFirstName();
@@ -137,6 +173,27 @@ public class AuthorizedProfileViewModel extends BaseObservable {
             @Override
             public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
                 Log.d("REZ", t.getMessage() != null?t.getMessage():"error");
+            }
+        });
+    }
+
+    public void changePassword(View view) {
+        Call<Void> call = ClientUtils.accountService.changePassword("Bearer " + token, new PasswordDto(getOldPassword(), getNewPassword()));
+        call.enqueue(new Callback<Void>() {
+            @Override
+            public void onResponse(@NonNull Call<Void> call, @NonNull Response<Void> response) {
+                Log.d("Status Code", String.valueOf(response.code()));
+                setOldPassword("");
+                setNewPassword("");
+                setRepeatPassword("");
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<Void> call, @NonNull Throwable t) {
+                Log.d("REZ", t.getMessage() != null?t.getMessage():"error");
+                setOldPassword("");
+                setNewPassword("");
+                setRepeatPassword("");
             }
         });
     }
