9ed168432b748f52e0f68e104717a6d1f01f38a1
Teodor Vidakovic <teodorv22@gmail.com>
2023-12-16 14:57:34 +0100

    feat: add datePicker range

--- app/src/main/java/com/example/accommodiq/adapters/AvailabilityRangeListAdapter.java
+++ app/src/main/java/com/example/accommodiq/adapters/AvailabilityRangeListAdapter.java
@@ -3,6 +3,7 @@ package com.example.accommodiq.adapters;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
@@ -12,7 +13,10 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.accommodiq.R;
 import com.example.accommodiq.models.Availability;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class AvailabilityRangeListAdapter extends RecyclerView.Adapter<AvailabilityRangeListAdapter.ViewHolder> {
     private List<Availability> availabilityRangeList;
@@ -31,12 +35,15 @@ public class AvailabilityRangeListAdapter extends RecyclerView.Adapter<Availabil
     @Override
     public void onBindViewHolder(@NonNull AvailabilityRangeListAdapter.ViewHolder holder, int position) {
         Availability availability = availabilityRangeList.get(position);
-        holder.textViewFromDate.setText(Math.toIntExact(availability.getFromDate()));
-        holder.textViewToDate.setText(Math.toIntExact(availability.getToDate()));
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        holder.textViewFromDate.setText(dateFormat.format(new Date(availability.getFromDate())));
+        holder.textViewToDate.setText(dateFormat.format(new Date(availability.getToDate())));
         holder.textViewPrice.setText(String.valueOf(availability.getPrice()));
         holder.buttonDelete.setOnClickListener(view -> {
-            availabilityRangeList.remove(position);
-            notifyItemRemoved(position);
+            int positionToRemove = holder.getAdapterPosition();
+            if (positionToRemove != RecyclerView.NO_POSITION) {
+                removeItem(positionToRemove);
+            }
         });
     }
 
@@ -45,16 +52,29 @@ public class AvailabilityRangeListAdapter extends RecyclerView.Adapter<Availabil
         return availabilityRangeList != null ? availabilityRangeList.size() : 0;
     }
 
+    public void addItem(Availability availability) {
+        availabilityRangeList.add(availability);
+        notifyItemInserted(availabilityRangeList.size() - 1);
+    }
+
+    public void removeItem(int position) {
+        if (position < 0 || position >= availabilityRangeList.size()) {
+            return; // Out of bounds
+        }
+        availabilityRangeList.remove(position);
+        notifyItemRemoved(position);
+    }
+
     public static class ViewHolder extends RecyclerView.ViewHolder {
         TextView textViewFromDate;
         TextView textViewToDate;
         TextView textViewPrice;
-        ImageButton buttonDelete;
+        Button buttonDelete;
 
         public ViewHolder(View itemView) {
             super(itemView);
-            textViewFromDate = itemView.findViewById(R.id.textViewStartDate);
-            textViewToDate = itemView.findViewById(R.id.textViewEndDate);
+            textViewFromDate = itemView.findViewById(R.id.textViewFromDate);
+            textViewToDate = itemView.findViewById(R.id.textViewToDate);
             textViewPrice = itemView.findViewById(R.id.textViewPrice);
             buttonDelete = itemView.findViewById(R.id.buttonDelete);
         }
--- app/src/main/java/com/example/accommodiq/ui/newAccommodation/NewAccommodation.java
+++ app/src/main/java/com/example/accommodiq/ui/newAccommodation/NewAccommodation.java
@@ -1,5 +1,6 @@
 package com.example.accommodiq.ui.newAccommodation;
 
+import androidx.core.util.Pair;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -7,27 +8,45 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.example.accommodiq.R;
 import com.example.accommodiq.adapters.AvailabilityRangeListAdapter;
 import com.example.accommodiq.databinding.FragmentNewAccommodationBinding;
 import com.example.accommodiq.models.Availability;
+import com.google.android.material.datepicker.MaterialDatePicker;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
 
 public class NewAccommodation extends Fragment {
 
+    private NewAccommodationViewModel newAccommodationViewModel;
     private FragmentNewAccommodationBinding binding;
+    private AvailabilityRangeListAdapter adapter;
+    private List<Availability> availabilityList = new ArrayList<>();
+    private Long selectedFromDate;
+    private Long selectedToDate;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        NewAccommodationViewModel newAccommodationViewModel = new ViewModelProvider(this).get(NewAccommodationViewModel.class);
+
+        newAccommodationViewModel = new ViewModelProvider(this).get(NewAccommodationViewModel.class);
 
         binding = FragmentNewAccommodationBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
+
+        adapter = new AvailabilityRangeListAdapter(availabilityList);
 
         return root;
     }
@@ -35,15 +54,81 @@ public class NewAccommodation extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Set up RecyclerView with the adapter
-        AvailabilityRangeListAdapter adapter = new AvailabilityRangeListAdapter([]);
-        binding.recyclerViewPriceRange.setAdapter(adapter);
+
+        // Sample date initialization, will be changed later
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        Date startDate1, endDate1, startDate2, endDate2;
+        try {
+            startDate1 = dateFormat.parse("03/12/2023");
+            endDate1 = dateFormat.parse("05/12/2023");
+            startDate2 = dateFormat.parse("10/12/2023");
+            endDate2 = dateFormat.parse("15/12/2023");
+        } catch (ParseException e) {
+            e.printStackTrace();
+            // Handle the error according to your needs
+            return;
+        }
+
+        availabilityList.add(new Availability(1L, startDate1.getTime(), endDate1.getTime(), 100.00));
+        availabilityList.add(new Availability(2L, startDate2.getTime(), endDate2.getTime(), 150.00));
+
+        AvailabilityRangeListAdapter adapter = new AvailabilityRangeListAdapter(availabilityList);
+        binding.recyclerViewAvailabilityRange.setLayoutManager(new LinearLayoutManager(getContext()));
+        binding.recyclerViewAvailabilityRange.setAdapter(adapter);
+
+        binding.editTextSelectRange.setOnClickListener(v -> showMaterialDateRangePicker());
+
+        binding.buttonAdd.setOnClickListener(v -> {
+            if (selectedFromDate != null && selectedToDate != null) {
+                String priceString = binding.editTextPrice.getText().toString();
+                double price;
+                try {
+                    price = Double.parseDouble(priceString);
+                } catch (NumberFormatException e) {
+                    Toast.makeText(getContext(), "Invalid price entered", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (price > 0) {
+                    Availability newAvailability = new Availability(-1L, selectedFromDate, selectedToDate, price);
+                    adapter.addItem(newAvailability);
+                } else {
+                    Toast.makeText(getContext(), "Price must be greater than 0", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Toast.makeText(getContext(), "Please select a date range first.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    private void showMaterialDateRangePicker() {
+        MaterialDatePicker.Builder<Pair<Long, Long>> builder = MaterialDatePicker.Builder.dateRangePicker();
+        builder.setTitleText("Select Date Range");
+        final MaterialDatePicker<Pair<Long, Long>> picker = builder.build();
+
+        picker.addOnPositiveButtonClickListener(selection -> {
+            Pair<Long, Long> dateRange = selection;
+            if (dateRange.first != null && dateRange.second != null) {
+                selectedFromDate = dateRange.first;
+                selectedToDate = dateRange.second;
+                String fromDate = formatDate(dateRange.first);
+                String toDate = formatDate(dateRange.second);
+                binding.editTextSelectRange.setText(String.format("%s - %s", fromDate, toDate));
+            }
+        });
+
+        picker.show(getChildFragmentManager(), picker.toString());
+    }
+
+    private String formatDate(Long dateInMillis) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        return dateFormat.format(new Date(dateInMillis));
     }
 
 }
