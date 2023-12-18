b1ce3d293a825641655bc85552b882cd0c917937
Teodor Vidakovic <teodorv22@gmail.com>
2023-12-18 13:16:29 +0100

    feat: implement api service endpoints and needed Dtos

--- app/src/main/java/com/example/accommodiq/services/interfaces/AccommodationApiService.java
+++ app/src/main/java/com/example/accommodiq/services/interfaces/AccommodationApiService.java
@@ -1,14 +1,23 @@
 package com.example.accommodiq.services.interfaces;
 
+import com.example.accommodiq.dtos.AccommodationBookingDetailFormDto;
+import com.example.accommodiq.dtos.AccommodationBookingDetailsDto;
 import com.example.accommodiq.dtos.AccommodationCreateDto;
 import com.example.accommodiq.dtos.AccommodationDetailsDto;
+import com.example.accommodiq.dtos.AvailabilityDto;
+import com.example.accommodiq.dtos.MessageDto;
+import com.example.accommodiq.models.Availability;
 
 import java.util.List;
 
 import okhttp3.MultipartBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface AccommodationApiService {
     @POST("/images")
@@ -16,4 +25,16 @@ public interface AccommodationApiService {
 
     @POST("hosts/accommodations")
     Call<AccommodationDetailsDto> createNewAccommodation(@Body AccommodationCreateDto accommodation);
+
+    @PUT("/accommodations/{accommodationId}/booking-details")
+    Call<AccommodationDetailsDto> updateAccommodationBookingDetails(@Path("accommodationId") Long accommodationId, @Body AccommodationBookingDetailsDto updateDto);
+
+    @GET("/accommodations/{accommodationId}/booking-details")
+    Call<AccommodationBookingDetailFormDto> getAccommodationBookingDetails(@Path("accommodationId") Long accommodationId);
+
+    @POST("/accommodations/{accommodationId}/availabilities")
+    Call<List<Availability>> addAccommodationAvailability(@Path("accommodationId") Long accommodationId, @Body AvailabilityDto availabilityDto);
+
+    @DELETE("/accommodations/{accommodationId}/availabilities/{availabilityId}")
+    Call<MessageDto> removeAccommodationAvailability(@Path("accommodationId") Long accommodationId, @Path("availabilityId") Long availabilityId);
 }
--- app/src/main/java/com/example/accommodiq/ui/updateAccommodationAvailability/UpdateAccommodationAvailabilityViewModel.java
+++ app/src/main/java/com/example/accommodiq/ui/updateAccommodationAvailability/UpdateAccommodationAvailabilityViewModel.java
@@ -1,7 +1,67 @@
 package com.example.accommodiq.ui.updateAccommodationAvailability;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.example.accommodiq.dtos.AccommodationBookingDetailFormDto;
+import com.example.accommodiq.dtos.AccommodationBookingDetailsDto;
+import com.example.accommodiq.dtos.AccommodationDetailsDto;
+import com.example.accommodiq.dtos.AvailabilityDto;
+import com.example.accommodiq.dtos.MessageDto;
+import com.example.accommodiq.models.Availability;
+import com.example.accommodiq.services.interfaces.AccommodationApiService;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 public class UpdateAccommodationAvailabilityViewModel extends ViewModel {
-    // TODO: Implement the ViewModel
+    private MutableLiveData<List<Availability>> availabilityListLiveData;
+    private AccommodationApiService apiService;
+
+    public UpdateAccommodationAvailabilityViewModel() {
+        availabilityListLiveData = new MutableLiveData<>();
+        initApiService();
+        // Optionally load initial data here if needed
+    }
+
+    private void initApiService() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://localhost:8000/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        apiService = retrofit.create(AccommodationApiService.class);
+    }
+
+    public LiveData<List<Availability>> getAvailabilityListLiveData() {
+        return availabilityListLiveData;
+    }
+
+    public void setAvailabilityList(List<Availability> availabilityList) {
+        availabilityListLiveData.setValue(availabilityList);
+    }
+
+    public void updateAccommodationBookingDetails(Long accommodationId,AccommodationBookingDetailsDto updateDto, Callback<AccommodationDetailsDto> callback) {
+        Call<AccommodationDetailsDto> call = apiService.updateAccommodationBookingDetails(accommodationId, updateDto);
+        call.enqueue(callback);
+    }
+
+    public void getAccommodationBookingDetails(Long accommodationId, Callback<AccommodationBookingDetailFormDto> callback) {
+        Call<AccommodationBookingDetailFormDto> call = apiService.getAccommodationBookingDetails(accommodationId);
+        call.enqueue(callback);
+    }
+
+    public void addAccommodationAvailability(Long accommodationId, AvailabilityDto availabilityDto, Callback<List<Availability>> callback) {
+        Call<List<Availability>> call = apiService.addAccommodationAvailability(accommodationId,availabilityDto);
+        call.enqueue(callback);
+    }
+
+    public void removeAccommodationAvailability(Long accommodationId, Long availabilityId, Callback<MessageDto> callback) {
+        Call<MessageDto> call = apiService.removeAccommodationAvailability(accommodationId,availabilityId);
+        call.enqueue(callback);
+    }
 }
