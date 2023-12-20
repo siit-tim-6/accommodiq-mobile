1d8a2c081444ea0df64729a63980594894cbdf95
Teodor Vidakovic <teodorv22@gmail.com>
2023-12-20 01:43:19 +0100

    feat: add JwtUtils class

---
+++ app/src/main/java/com/example/accommodiq/Utility/JwtUtils.java
@@ -0,0 +1,21 @@
+package com.example.accommodiq.Utility;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class JwtUtils {
+    private static final String PREF_NAME = "jwt_pref";
+    private static final String KEY_JWT = "jwt_key";
+
+    public static void saveJwt(Context context, String jwt) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(KEY_JWT, jwt);
+        editor.apply();
+    }
+
+    public static String getJwt(Context context) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        return sharedPreferences.getString(KEY_JWT, null);
+    }
+}
