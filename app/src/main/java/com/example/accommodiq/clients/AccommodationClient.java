38a1cac1ce3f0b40e5347b95d2535d6acf99376b
Vladimir Popov <vladapopov02@gmail.com>
2023-12-24 18:27:00 +0100

    feat: update client to work with lists

--- app/src/main/java/com/example/accommodiq/clients/AccommodationClient.java
+++ app/src/main/java/com/example/accommodiq/clients/AccommodationClient.java
@@ -1,2 +1,30 @@
-package com.example.accommodiq.clients;public interface AccommodationClient {
+package com.example.accommodiq.clients;
+
+import com.example.accommodiq.dtos.AccommodationListDto;
+import com.example.accommodiq.models.Accommodation;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Headers;
+import retrofit2.http.Part;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface AccommodationClient {
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type: application/json"
+    })
+    @GET("accommodations")
+    Call<List<AccommodationListDto>> getAccommodations(@Query("title") String title, @Query("location") String location, @Query("availableFrom") Long availableFrom, @Query("availableTo") Long availableTo,
+                                                      @Query("priceFrom") Integer priceFrom, @Query("priceTo") Integer priceTo, @Query("guests") Integer guests, @Query("type") String type, @Query("benefits") String benefits);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type: application/json"
+    })
+    @GET("accommodations/{accommodationId}")
+    Call<AccommodationListDto> getAccommodationDetails(@Path("accommodationId") long accommodationId);
 }
