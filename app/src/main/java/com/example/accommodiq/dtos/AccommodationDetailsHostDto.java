178808c4563ff09400a14f9b1ce46c78b01a02e5
Vladimir Popov <vladapopov02@gmail.com>
2023-12-25 01:07:34 +0100

    feat: add constructor and getters/setters

--- app/src/main/java/com/example/accommodiq/dtos/AccommodationDetailsHostDto.java
+++ app/src/main/java/com/example/accommodiq/dtos/AccommodationDetailsHostDto.java
@@ -5,4 +5,13 @@ public class AccommodationDetailsHostDto {
     private String name;
     private double rating;
     private int reviewCount;
+
+    public AccommodationDetailsHostDto() { }
+
+    public AccommodationDetailsHostDto(long id, String name, double rating, int reviewCount) {
+        this.id = id;
+        this.name = name;
+        this.rating = rating;
+        this.reviewCount = reviewCount;
+    }
 }
--- app/src/main/java/com/example/accommodiq/dtos/AccommodationDetailsReviewDto.java
+++ app/src/main/java/com/example/accommodiq/dtos/AccommodationDetailsReviewDto.java
@@ -5,4 +5,45 @@ public class AccommodationDetailsReviewDto {
     private String comment;
     private double rating;
     private long date;
+
+    public AccommodationDetailsReviewDto() { }
+
+    public AccommodationDetailsReviewDto(String author, String comment, double rating, long date) {
+        this.author = author;
+        this.comment = comment;
+        this.rating = rating;
+        this.date = date;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
 }
