8d04f059abeb871a62ba3a68686a69e330834efc
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-01-16 04:18:57 +0100

    feat: Create review approval card

--- app/src/main/java/com/example/accommodiq/dtos/ReviewApprovalDto.java
+++ app/src/main/java/com/example/accommodiq/dtos/ReviewApprovalDto.java
@@ -8,7 +8,56 @@ public class ReviewApprovalDto {
     String status;
     long authorId;
     String author;
+
     public ReviewApprovalDto() {
+    }
+
+    public float getRating() {
+        return (float) rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public long getDate() {
+        return date;
+    }
+
+    public void setDate(long date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public long getAuthorId() {
+        return authorId;
+    }
+
+    public void setAuthorId(long authorId) {
+        this.authorId = authorId;
+    }
+
+    public String getAuthor() {
+        return "Author: " + author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
     }
 
     public long getId() {
