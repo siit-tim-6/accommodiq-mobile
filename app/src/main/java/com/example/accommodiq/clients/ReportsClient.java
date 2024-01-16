6f8c810df3b0ce640b60e4a0830729510a27e2f0
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-01-16 02:33:11 +0100

    feat: Create report endpoints and bind them to buttons

--- app/src/main/java/com/example/accommodiq/clients/ReportsClient.java
+++ app/src/main/java/com/example/accommodiq/clients/ReportsClient.java
@@ -1,12 +1,17 @@
 package com.example.accommodiq.clients;
 
+import com.example.accommodiq.dtos.AccountStatusDto;
 import com.example.accommodiq.dtos.UserReportDto;
 
 import java.util.ArrayList;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.Headers;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
 
 public interface ReportsClient {
     @Headers({
@@ -15,4 +20,18 @@ public interface ReportsClient {
     })
     @GET("reports")
     Call<ArrayList<UserReportDto>> getReports();
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type: application/json"
+    })
+    @PUT("users/{id}/status")
+    Call<Void> blockUser(@Path("id") long id, @Body AccountStatusDto status);
+
+    @Headers({
+            "User-Agent: Mobile-Android",
+            "Content-Type: application/json"
+    })
+    @DELETE("reports/{id}")
+    Call<Void> deleteReport(@Path("id") long id);
 }
--- app/src/main/java/com/example/accommodiq/ui/userReport/UserReportListFragment.java
+++ app/src/main/java/com/example/accommodiq/ui/userReport/UserReportListFragment.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,48 +11,17 @@ import androidx.fragment.app.ListFragment;
 
 import com.example.accommodiq.R;
 import com.example.accommodiq.adapters.UserReportsListAdapter;
-import com.example.accommodiq.apiConfig.RetrofitClientInstance;
-import com.example.accommodiq.clients.ReportsClient;
-import com.example.accommodiq.dtos.UserReportDto;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class UserReportListFragment extends ListFragment {
-    private UserReportsListAdapter adapter;
-    private List<UserReportDto> reports;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        getReports();
+        UserReportsListAdapter adapter = new UserReportsListAdapter(getContext(), new ArrayList<>());
+        setListAdapter(adapter);
         return inflater.inflate(R.layout.fragment_user_report_list, container, false);
     }
 
-    private void getReports() {
-        ReportsClient reportsClient = RetrofitClientInstance.getRetrofitInstance(getContext()).create(ReportsClient.class);
-
-        Call<ArrayList<UserReportDto>> call = reportsClient.getReports();
-        call.enqueue(new Callback<ArrayList<UserReportDto>>() {
-            @Override
-            public void onResponse(@NonNull Call<ArrayList<UserReportDto>> call, @NonNull Response<ArrayList<UserReportDto>> response) {
-                if (response.isSuccessful()) {
-                    reports = response.body();
-                    adapter = new UserReportsListAdapter(getContext(), (ArrayList<UserReportDto>) reports);
-                    setListAdapter(adapter);
-                } else {
-                    Toast.makeText(getContext(), "Error: " + response.code(), Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            @Override
-            public void onFailure(@NonNull Call<ArrayList<UserReportDto>> call, @NonNull Throwable t) {
-                Toast.makeText(getContext(), "Error: " + t.getMessage(), Toast.LENGTH_SHORT).show();
-            }
-        });
-    }
 }
