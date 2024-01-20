db7485873e10a8f54ddb3942e73ccb364bfb3436
BulatovicBalsa <baki.b.2k02@gmail.com>
2024-01-20 02:49:59 +0100

    feat: Change admin accommodation review destination

--- app/src/main/java/com/example/accommodiq/ui/accommodationReview/AccommodationReviewFragment.java
+++ app/src/main/java/com/example/accommodiq/ui/accommodationReview/AccommodationReviewFragment.java
@@ -5,41 +5,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
+import androidx.fragment.app.ListFragment;
 import androidx.navigation.Navigation;
 
 import com.example.accommodiq.R;
-import com.example.accommodiq.databinding.FragmentAccommodationReviewBinding;
+import com.example.accommodiq.adapters.AccommodationListAdapter;
+import com.example.accommodiq.apiConfig.RetrofitClientInstance;
+import com.example.accommodiq.clients.AccommodationClient;
+import com.example.accommodiq.dtos.AccommodationListDto;
 import com.example.accommodiq.enums.AccommodationListType;
-import com.example.accommodiq.fragments.AccommodationsListFragment;
-import com.example.accommodiq.fragments.FragmentTransition;
 
-public class AccommodationReviewFragment extends Fragment {
-    private FragmentAccommodationReviewBinding binding;
+import java.util.ArrayList;
+import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+public class AccommodationReviewFragment extends ListFragment {
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        binding = FragmentAccommodationReviewBinding.inflate(inflater, container, false);
-        return binding.getRoot();
-    }
+        AccommodationClient accommodationClient = RetrofitClientInstance.getRetrofitInstance(requireContext()).create(AccommodationClient.class);
+        accommodationClient.getPendingAccommodations().enqueue(new Callback<List<AccommodationListDto>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<AccommodationListDto>> call, @NonNull Response<List<AccommodationListDto>> response) {
+                if (!response.isSuccessful()) {
+                    Toast.makeText(requireContext(), "Error: " + response.code(), Toast.LENGTH_LONG).show();
+                    return;
+                }
+                assert response.body() != null;
+                setListAdapter(new AccommodationListAdapter(requireContext(), (ArrayList<AccommodationListDto>) response.body(), AccommodationListType.ADMIN_REVIEW_PENDING_ACCOMMODATIONS));
+            }
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        Activity mainActivity = getActivity();
-        if (mainActivity != null) {
-            //FragmentTransition.to(AccommodationsListFragment.newInstance(getActivity(), AccommodationListType.ADMIN_REVIEW_PENDING_ACCOMMODATIONS), getActivity(), true, R.id.admin_accommodation_review_fragment);
-            Bundle bundle = new Bundle();
-            bundle.putSerializable("accommodationListType", AccommodationListType.ADMIN_REVIEW_PENDING_ACCOMMODATIONS);
-            Navigation.findNavController(requireView()).navigate(R.id.action_accommodationReview_to_accommodationsListFragment, bundle);
-        }
+            @Override
+            public void onFailure(@NonNull Call<List<AccommodationListDto>> call, @NonNull Throwable t) {
+                Toast.makeText(requireContext(), "Error: " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+        return inflater.inflate(R.layout.fragment_accommodation_review, container, false);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        binding = null;
     }
 }
